3fcd5c8aa883446e4b3dc87a95807c0a79e649e4
verse215@gmail.com <verse215@gmail.com>
2015-11-01 11:33:23 -0500

    Updates

--- Java/RandomCode/LeastCommonAncestorBST/LCAofBST.java
+++ Java/RandomCode/LeastCommonAncestorBST/LCAofBST.java
@@ -1,27 +1,56 @@
 class LCAofBST{
 
-   private class Node{
+   static class Node{
       int data;
       Node left;
       Node right;
    }
 
-   private Node buildTree(){
+   private void buildTree(Node root){
+      for(int i = 0; i < 16; i++){
+         insertNode(root,i);
+      }
    }
-   
+  
+   private void printTree(Node root){
+      if(root == null){return;}
+      
+      if(root.left !=null){
+         System.out.println(root.data);
+         printTree(root.left);
+      }
+      if(root.right != null){
+         System.out.println(root.data);
+         printTree(root.right);
+      }
+   }
+ 
    private void insertNode(Node root, int data){
-      Node node = new Node();
-      node.data = data;
+      Node temp = new Node();
+      temp.data = data;
 
+      Node parent;
       while(root != null){
+         parent = root;
          if(root.data > data){
             root = root.left;
+            if(root == null){parent.left = temp; break;}
          }
          else{
+
             root = root.right;
+            if(root == null){parent.right = temp; break;}
          }
       }
+   }
+ 
+   public static void main(String[] args){
+      Node root = new Node();
+      root.data = -1;
 
-      root = node;
+      LCAofBST lca = new LCAofBST();  
+      lca.buildTree(root);
+      lca.printTree(root);
+    
    }
 }
