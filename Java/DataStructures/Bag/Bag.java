a9880a6dc46810611c62abd7677eb76a963530f0
verse215 <verse215@gmail.com>
2016-04-06 20:12:38 -0400

    Good Bag

--- Java/DataStructures/Bag/Bag.java
+++ Java/DataStructures/Bag/Bag.java
@@ -1,24 +1,63 @@
+import java.util.Iterator;
 /**
 * Implementation of a Bag data structure
 */
-class Bag <Item>{
+class Bag <Item> implements Iterable<Item>{
+
    // Array to store the contents of the Bag
    private Item[] bag;
+   private int count;
 
    /**
    * The contstructor for the Bag
    * accepts a size parameter.
    *
    **/
-   public Bag(int size){
+   public Bag(int size) {
       
       // Set the size of the array of items
       // suppress the warning for 
-      @SuppressWarnings("unchecked")
       bag = (Item[])new Object[size];
+      count = 0;
    }
 
+   /**
+   * Add an item to the bag
+   **/
    public void add(Item item){
+      bag[count++] = item; 
+   }
+
+   /**
+   * Get the number of items in the bag
+   **/ 
+   public int count() {
+      return count;
+   }
+   /**
+   * Return an iterator for the bag
+   **/
+   public Iterator<Item> iterator() {
+      return new bagIterator();
+   }
+
+   private class bagIterator implements Iterator<Item> {
       
+      // Current place in the bag container
+      int currPointer;
+      
+      public bagIterator() {
+         currPointer = 0;
+      }
+
+      public boolean hasNext() {
+         return currPointer < count;
+      }
+      
+      public Item next() {
+         return bag[currPointer++];
+      }
+
+      public void remove(){}
    }
 }
