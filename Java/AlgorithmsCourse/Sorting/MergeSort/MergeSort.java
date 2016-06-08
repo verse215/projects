225432c0a103f1374d349304204394c7f1ca54ef
verse215 <verse215@gmail.com>
2016-06-07 22:16:45 -0400

    update merge sort to actaully work

--- Java/AlgorithmsCourse/Sorting/MergeSort/MergeSort.java
+++ Java/AlgorithmsCourse/Sorting/MergeSort/MergeSort.java
@@ -4,16 +4,17 @@ class MergeSort{
    private int[] values;
    private int[] temp;
 
-   public MergeSort(int[] values){
+   public void sort(int[] values){
       this.values = values;
       this.temp = new int[values.length];
+      sort(0,values.length-1);
    } 
    
    public void sort(int begin, int end){
       if( begin < end){
          int mid = begin + (end - begin)/2;
-         sort(values[begin],values[mid]);
-         sort(values[mid + 1],values[end]);
+         sort(begin,mid);
+         sort(mid + 1,end);
          merge(begin,mid,end);
       }
    }
@@ -25,8 +26,32 @@ class MergeSort{
      
       int i = begin;
       int j = mid + 1;
+      
+      for(int a = begin; a < end; a++) {
+         if(i > mid)                values[a] = temp[j++];
+         else if(j > end)           values[a] = temp[i++];
+         else if(temp[i] < temp[j]) values[a] = temp[i++];
+         else                       values[a] = temp[j++];
+      }
+   }
+
+   /**
+   * Alternate version
+   * I like the other way better
+   * the purpose of the last while
+   * loop in this method is confusing
+   * As in, why don't we have to do the 
+   * same with 'j'
+   **/
+   private void merge2(int begin, int mid, int end){
+      for(int i = begin; i <= end; i++){
+         temp[i] = values[i];
+      }
+     
+      int i = begin;
+      int j = mid + 1;
       int k = begin;
-   
+      
       while(i <= mid && j <= end){
          if(temp[i] <= temp[j]){
            values[k] = temp[i];
---
+++ Java/AlgorithmsCourse/Sorting/MergeSort/MergeSortTest.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.Before;
+
+public class MergeSortTest {
+
+   int[] toSort;
+
+   private boolean isSorted(int[] someArray) {
+      int num = someArray[0];
+      for(int i = 1; i < someArray.length; i++) {
+         if(num > someArray[i]) return false;
+         num = someArray[i];
+      }
+
+      return true;
+   }
+
+   @Before
+   public void setup() {
+      Random rand = new Random();
+      toSort = new int[1000000];
+      for(int i = 0; i < toSort.length; i++) {
+         toSort[i] = rand.nextInt();
+      }
+   }
+
+   @Test
+   public void test1() {
+      MergeSort sort = new MergeSort();
+      sort.sort(toSort);
+   }
+}
